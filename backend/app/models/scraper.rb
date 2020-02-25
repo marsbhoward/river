aa19f42c7506c7a538a14fae29893da2d9a7652f
Mars <marsbhoward@gmail.com>
2020-02-24 19:32:41 -0500

    install react

--- backend/app/models/scraper.rb
+++ backend/app/models/scraper.rb
@@ -1,2 +1,19 @@
 class Scraper < ApplicationRecord
+	def get_movies(stream)
+		current_stream = Stream.find_by(name:stream.name)
+
+		
+		movies = Nokogiri::HTML(open(current_stream.url)).css("title-list-grid__item")
+
+
+		movie_collection = []
+		movie_data = []
+
+		movies.each do |post|
+			movie_data.push (post)
+		end
+
+
+		return movie_collection
+	end
 end
